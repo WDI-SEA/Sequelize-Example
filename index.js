dfad03ff86b4d562455ac5fe4ded2b9f996300a1
darkenvy <darkenvy6@gmail.com>
2017-04-19 08:53:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+var db = require('./models');
+
+// --------- //
+//     C     //
+// --------- //
+// db.user.create({
+//   firstName: 'Reno',
+//   lastName: 'McKenie',
+//   age: 26
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+
+// db.user.findOrCreate({
+//   where: {
+//     firstName: 'Paul',
+//     lastName: 'Rudd'
+//   },
+//   defaults: {age: 47}
+// }).spread(function(user, created) {
+//   console.log(user);
+// });
+
+// --------- //
+//     R     //
+// --------- //
+// db.user.findAll().then(function(users){
+//   console.log(users);
+// });
+
+// --------- //
+//     U     //
+// --------- //
+// db.user.update({
+//   lastName: 'Taco'
+// }, {
+//   where: {
+//     firstName: 'Reno'
+//   }
+// }).then(function(user) {
+//   console.log(user);
+// });
+
+// --------- //
+//     D     //
+// --------- //
+// db.user.destroy({
+//   where: { firstName: 'Reno'}
+// }).then(function() {
+//   console.log('Deleted')
+// });
+
+
+// ---------------- //
+//     Promises     //
+// ---------------- //
+
+// Find a user
+// db.user.find({
+//   where: {id: 1}
+// })
+
+// Find a user and do something with it
+// db.user.findById(1).then(function(foundUser){
+//   console.log(foundUser);
+// });
+
+
+// findOrCreate returns an array. And therefore requires something else
+// It's Promise-like, but it's called spread
+// db.user.findOrCreate({
+//   where: { firstName: 'Peter' }
+// }).spread(function(user, created) {
+//   console.log(user); // returns info about the user
+// });
+
+
+// --------------- //
+//    Validation   //
+// --------------- //
+
+// db.user.create({
+//   firstName: 'Reno',
+//   lastName: 'McKenie',
+//   age: 26,
+//   email: 'renohacks.com'
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+// db.user.create({
+//   firstName: 'Reno',
+//   lastName: 'McKenie',
+//   age: 26,
+//   email: 'darkenvy@protonmail.com'
+// }).then(function(data) {
+//   console.log(data);
+// });
